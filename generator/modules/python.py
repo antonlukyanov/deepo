7d8124b61d0e3830de74c108b84ea0188dfd1c6e
Anton Lukyanov <anton.lukyanov@gmail.com>
2019-03-22 00:14:48 +0300

    Updated Boost to 1.69.0 and added support for Python 3.7.

--- generator/modules/python.py
+++ generator/modules/python.py
@@ -4,38 +4,39 @@ from .tools import Tools
 
 
 @dependency(Tools)
-@version('3.6')
+@version('3.7')
 @source('apt')
 class Python(Module):
 
     def __init__(self, manager, **args):
         super(self.__class__, self).__init__(manager, **args)
-        if self.version not in ('2.7', '3.6',):
+        if self.version not in ('2.7', '3.7', '3.6'):
             raise NotImplementedError('unsupported python version')
 
     def build(self):
-        return (r'''
+        pyver = self.version
+        return (rf'''
             DEBIAN_FRONTEND=noninteractive $APT_INSTALL \
                 software-properties-common \
                 && \
             add-apt-repository ppa:deadsnakes/ppa && \
             apt-get update && \
             DEBIAN_FRONTEND=noninteractive $APT_INSTALL \
-                python3.6 \
-                python3.6-dev \
+                python{pyver} \
+                python{pyver}-dev \
                 python3-distutils%s \
                 && \
             wget -O ~/get-pip.py \
                 https://bootstrap.pypa.io/get-pip.py && \
-            python3.6 ~/get-pip.py && \
-            ln -s /usr/bin/python3.6 /usr/local/bin/python3 && \
-            ln -s /usr/bin/python3.6 /usr/local/bin/python && \
+            python{pyver} ~/get-pip.py && \
+            ln -s /usr/bin/python{pyver} /usr/local/bin/python3 && \
+            ln -s /usr/bin/python{pyver} /usr/local/bin/python && \
             $PIP_INSTALL \
                 setuptools \
                 && \
             ''' % (
                 '' if self.composer.ubuntu_ver.startswith('18.') else '-extra'
-            ) if self.version == '3.6' else
+            ) if self.version in ('3.7', '3.6') else
             r'''
             DEBIAN_FRONTEND=noninteractive $APT_INSTALL \
                 python-pip \
